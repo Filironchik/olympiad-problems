37a937cda1cec214be0f610106dcf36877c097fe
Filiron-Сова Айтишник <78251890+Filironchik@users.noreply.github.com>
2023-04-07 13:13:38 +0000

    123

---
+++ task/task3.ts
@@ -0,0 +1,15 @@
+import * as readline from 'readline';
+
+let rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question('Введите позицию числа: ', (answer) => {
+    let number:number = Number(answer.toLowerCase());
+
+    let dataString: string = "";
+
+    for(let i:number = 0; i <= 100; i++){dataString = dataString + String(i*i)}
+
+    console.log(dataString[number])
+
+    rl.close();
+});
---
+++ task/task4.ts
@@ -0,0 +1,34 @@
+import * as readline from 'readline';
+
+let rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+let numbers:number[] = [];
+
+
+rl.question("Введите последоватеьность чисел через пробел: ", (answer) => {
+    answer = answer + " "
+    
+    let num: string = "";
+    let productNumbers: number[] = [];
+
+    for(let i = 0; i <= answer.length; i++){
+        if(answer[i] !== " "){
+            num = num + answer[i];
+        } else {
+            numbers.push(Number(num))
+            num = "";
+        }
+    }
+
+    for(let i: number = 0; i <= numbers.length; i++){
+        for (let a: number = i + 1; a <= numbers.length - 1; a++){
+            productNumbers.push(numbers[i] * numbers[a])
+        }
+    }
+
+    const sortNumbers: number[] = productNumbers.sort((n1,n2) => n2 - n1);
+    
+    console.log(sortNumbers[0]);
+
+    rl.close();
+});
